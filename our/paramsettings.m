6f330f3c0d86e7d2f25379e88479bcba8945db94
color image <64688189+Color-image@users.noreply.github.com>
2021-03-10 10:36:29 +0800

    Add files via upload

---
+++ our/paramsettings.m
@@ -0,0 +1,11 @@
+function params = paramsettings(varargin)
+params.beta = varargin{1};
+params.lambda = varargin{2};
+params.gama= varargin{3};
+params.eta = varargin{4};
+params.eta1 = varargin{5};
+params.Out_iter = varargin{6};
+params.errorGoal = varargin{7};
+params.sigma = varargin{8};
+params.patchsize = varargin{9};
+end
